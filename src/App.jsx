3064f8a459b3882d476657016c6ce227d3846263
adityaS1712 <89769748+adityaS1712@users.noreply.github.com>
2024-01-27 16:09:48 +0530

    context api

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Dashboard } from './components/Dashboard'
-import { Landing } from './components/Landing'
+import { useContext, useState } from "react"
+import { CountContext } from "./context";
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  
+  // wrap anyone that wants to use the teleported value inside a provider
   return (
-    <BrowserRouter>
-    <Routes>
-      <Route path="/dashboard" element={<Dashboard></Dashboard>} ></Route>
-      <Route path="/" element={<Landing></Landing>} ></Route>
-    </Routes>
-    </BrowserRouter>
+    <div>
+      <CountContext.Provider value={count}>
+        <Count setCount={setCount} />
+      </CountContext.Provider>
+    </div>
   )
+}
+
+function Count({setCount}) {
+  return <div>
+    <CountRenderer />
+    <Buttons setCount={setCount} />
+  </div>
+}
+
+function CountRenderer() {
+  const count = useContext(CountContext);
+
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons({setCount}) {
+  const count = useContext(CountContext);
+  return <div>
+    <button onClick={() => {
+      setCount(count + 1)
+    }}>Increase</button>
+
+    <button onClick={() => {
+      setCount(count - 1)
+    }}>Decrease</button>
+  </div>
 }
 
 export default App
